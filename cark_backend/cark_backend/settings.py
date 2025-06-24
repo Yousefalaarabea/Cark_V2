371b43cb4e74f9a51d25979ad7587b98fe6a5e24
Yousef Alaa <102681024+Yousefalaarabea@users.noreply.github.com>
2025-06-24 17:45:26 +0300

    Update settings.py

--- cark_backend/cark_backend/settings.py
+++ cark_backend/cark_backend/settings.py
@@ -138,8 +138,8 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'ssl': {
-                    'ca': '/home/site/wwwroot/cark_backend/certs/DigiCertGlobalRootCA.crt.pem',
-                },
+                'ca': os.path.join(BASE_DIR, 'DigiCertGlobalRootCA.crt.pem'),
+            }
         }
     }
 }
