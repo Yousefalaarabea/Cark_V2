be919fc3b72996dbd85e4faf9f9e0fbe24807c8b
Yousef Alaa <102681024+Yousefalaarabea@users.noreply.github.com>
2025-06-24 16:00:40 +0300

    Update settings.py

--- cark_backend/cark_backend/settings.py
+++ cark_backend/cark_backend/settings.py
@@ -14,13 +14,15 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import environ
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 SIMPLE_JWT = {
