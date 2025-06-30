750d934f60f2ec05a9722f341d6323638be1bdd3
Yousef Alaa <102681024+Yousefalaarabea@users.noreply.github.com>
2025-06-30 15:32:27 +0300

    Update general urls.py

--- cark_backend/cark_backend/urls.py
+++ cark_backend/cark_backend/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path('api/', include('cars.urls')),  # إضافة رابط الـ API للسيارات
     path('api/', include('documents.urls')),  # إضافة رابط الـ API للمستندات
     path('api/', include('rentals.urls')),  # إضافة رابط الـ API للايجارات
-    
+    path('api/', include('selfdrive_rentals.urls')),  # إضافة رابط الـ API للايجار بدون سائق
+    path('api/payments/', include('payments.urls')),  # إضافة رابط الـ API للمدفوعات
+    path('api/wallets/', include('wallets.urls')),  # إضافة رابط الـ API للمحفظة
 ]
 
  #أثناء التطوير فقط: السماح لخادم Django أن يخدم ملفات الميديا
