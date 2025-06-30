c62d724218f0e61e50b4a5d79dca6c64a4e61125
Yousef Alaa <102681024+Yousefalaarabea@users.noreply.github.com>
2025-06-30 16:20:41 +0300

    Update users urls.py

--- cark_backend/users/urls.py
+++ cark_backend/users/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RoleViewSet, UserRoleViewSet , RegisterView   , AssignRolesAPIView , UserRolesAPIView, UserListView
+from .views import RoleViewSet, UserRoleViewSet , RegisterView   , AssignRolesAPIView , UserRolesAPIView, UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'roles', RoleViewSet)
 router.register(r'user-roles', UserRoleViewSet)
+router.register(r'users', UserViewSet)
 
 
 
 
 urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
